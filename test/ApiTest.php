d4c8f2b0c7edd3cba2f58ed19f40b35640d7c5c1
Robin <robinpauquet@gmail.com>
2016-12-01 10:05:44 +0100

    error normalement

--- test/ApiTest.php
+++ test/ApiTest.php
@@ -13,7 +13,8 @@ class ApiTest extends TestCase
 	
 	public function testIfApiReturnGoodResult()
     {
-        $url="http://tecknologiks.com/money/api.php?curr=EUR&somme=100,EUR;200,EUR;300,EUR";
+        //$url="http://tecknologiks.com/money/api.php?curr=EUR&somme=100,EUR;200,EUR;300,EUR";
+		$url="http://tecknologiks.com/money/api.php?curr=EUR&somme=200,EUR;200,EUR;300,EUR";
 		$result = file_get_contents($url);
 		if ($result != '600') {
 			throw("RATE");
