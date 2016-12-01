f9299e6af1dd9e85e43b1f9af2905ed4e0f2877a
Robin <robinpauquet@gmail.com>
2016-12-01 12:02:33 +0100

    build ok

--- test/ApiTest.php
+++ test/ApiTest.php
@@ -13,14 +13,11 @@ class ApiTest extends TestCase
 		}
     }
 	
-	/**
-     * @covers            \SebastianBergmann\Money\Currency::__construct
-     * @expectedException \SebastianBergmann\Money\InvalidArgumentException
-     */
+	
 	public function testIfApiReturnGoodResult()
     {
-        //$url="http://tecknologiks.com/money/api.php?curr=EUR&somme=100,EUR;200,EUR;300,EUR";
-		$url="http://tecknologiks.com/money/api.php?curr=EUR&somme=200,EUR;200,EUR;300,EUR";
+        $url="http://tecknologiks.com/money/api.php?curr=EUR&somme=100,EUR;200,EUR;300,EUR";
+		//$url="http://tecknologiks.com/money/api.php?curr=EUR&somme=200,EUR;200,EUR;300,EUR";
 		$result = file_get_contents($url);
 		if ($result != '600') {
 			$this->expectError('Catastrophe');
